41a88f2bee945bda902219d2d71ba985d5bd2272
Dani neamtu <daniel.neamtu@plentymarkets.com>
2022-03-23 11:19:01 +0200

    Initialize empty plugin

--- resources/js/src/client.js
+++
@@ -1,9 +0,0 @@
-/**
- * This is our clientside javascript entry point
- */
-
-// The Ceres bundle provides Vue, we do not need to bundle it ourself
-const Vue = window.Vue;
-
-
-
--- resources/js/src/server.js
+++
@@ -1,4 +0,0 @@
-/**
- * This is our serverside javascript entry point
- */
-import Vue from "vue";
